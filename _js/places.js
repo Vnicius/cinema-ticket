68de06df46287228a2077b1d66c374a8f78d9466
Vinícius <vinicius.matheus252@gmail.com>
2017-06-19 22:04:02 -0300

    Places page

---
+++ _js/places.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  for (var i = 0; i < 20; i++){
+    $("#conteiner-places").append("<svg class=\"circle\" height=\"36\" width=\"36\">"+
+    "<circle cx=\"18\" cy=\"18\" r=\"15\" fill=\"gray\" /> </svg>")
+  }
+})
